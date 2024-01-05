a4807489e897bee4ec92be30253748d8fb489974
sachinsingh <sachinseengh@gmail.com>
2024-01-05 10:08:12 +0545

    Exit Functionality added on close button

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,34 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class MainController {
 	
+	 @FXML
+	    private Button exit_button;
+
+	    @FXML
+	    private Button login;
+
+	    @FXML
+	    private PasswordField password;
+
+	    @FXML
+	    private TextField username;
+
+	    @FXML
+	    void exit(ActionEvent event) {
+
+	    	
+	    	System.exit(0);
+	    }
+
+	    @FXML
+	    void login(ActionEvent event) {
+
+	    }
 }
